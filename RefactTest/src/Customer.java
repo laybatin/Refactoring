20d5cccbdf5727572d72690d5c187d47e1adc095
Olly <kim8706@gmail.com>
2014-01-11 23:10:33 +0900

    내가 실시한 리팩토링 2
    비디오 타입별로 값을 계산하는 메소드가 Customer클래스에 있으면 안된다는 것을 알았음에도 어디에다 둘지를 몰랐다.
    
    책을 보고 Rental클래스에 두어야 한다는 것을 알게되었다.

--- RefactTest/src/Customer.java
+++ RefactTest/src/Customer.java
@@ -33,7 +33,7 @@ public class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental)rentals.nextElement();
 			
-			thisAmount = videoTypeCalc(each);
+			thisAmount = each.getCharge(each);
 			
 			//적립 포인트를 1포인트 증가
 			frequentRenterPointers++;
@@ -57,28 +57,7 @@ public class Customer {
 				
 	}
 
-	private double videoTypeCalc(Rental each) {
-		double thisAmount = 0;
-		//비디오 종류별 대여료 계산
-		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			break;
-			
-		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
-			break;
-			
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
-	}
+	
 	
 	
 	

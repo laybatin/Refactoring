102170fe65ba9e05c553c0a8986eac922b9941f8
Olly <kim8706@gmail.com>
2014-01-11 23:22:07 +0900

    내가 실시한 리팩토링 3
    포인트 적립부분을 뺴서 Rental 클래스로 옮김
    중복된 변수를 제거
    
    난 포인트 적립부분을 빼서 Rental까지만 했었다.
    중복된 변수는 생각도 못했네... 습관이 안되
    
    그나저나 테스트 케이스 좀 편한듯..?
    리팩토링하고 테스트케이스 돌리고 확인하고를 반복해서
    리팩토링 시 버그가 있으면 바로 알 수 있어서
    편하다.
    
    하지만 이걸 위해서는 처음에 세팅을 잘 해놔야 겠지..

--- RefactTest/src/Customer.java
+++ RefactTest/src/Customer.java
@@ -30,23 +30,16 @@ public class Customer {
 		String result = getName() + " 고객님의 대여 기록\n";
 		
 		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = (Rental)rentals.nextElement();
 			
-			thisAmount = each.getCharge(each);
+			Rental each = (Rental)rentals.nextElement();			
 			
-			//적립 포인트를 1포인트 증가
-			frequentRenterPointers++;
-			
-			//최신물을 이틀 이상 대여하면 보너스 포인트 지급
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPointers++;
+			frequentRenterPointers += each.getPoint(each);
 			
 			//이번에 대여하는 비디오 정보와 대여료를 출력
-			result += "\t" + each.getMovie().getTitle()+"\t"+ String.valueOf(thisAmount) + "\n";
+			result += "\t" + each.getMovie().getTitle()+"\t"+ String.valueOf(each.getCharge(each)) + "\n";
 			
 			//현재까지 누적된 총 대여료
-			totalAmout += thisAmount;
+			totalAmout += each.getCharge(each);
 		}
 		
 		//푸터 행 추가
@@ -56,6 +49,8 @@ public class Customer {
 		
 				
 	}
+
+	
 
 	
 	
--- RefactTest/src/Rental.java
+++ RefactTest/src/Rental.java
@@ -42,5 +42,19 @@ public class Rental {
 		}
 		return result;
 	}
+	
+	public int getPoint(Rental each) {
+
+		int frequentRenterPointers = 0;
+		
+		//적립 포인트를 1포인트 증가
+		frequentRenterPointers++;
+		
+		//최신물을 이틀 이상 대여하면 보너스 포인트 지급
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+			frequentRenterPointers++;
+		
+		return frequentRenterPointers;
+	}
 
 }
